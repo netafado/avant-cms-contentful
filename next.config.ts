ea5c8f702c4e235b5a9e167550554da378b8b741
Isaias Santos <isaias.santos@boldcommerce.com>
2025-08-14 11:49:01 -0300

    feat: update Next.js configuration to include image remote patterns and disable caching in development

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+    ],
+  },
+  // Disable caching in development
+  ...(process.env.NODE_ENV === "development" && {
+    experimental: {
+      staleTimes: {
+        dynamic: 0,
+        static: 0,
+      },
+    },
+  }),
 };
 
 export default nextConfig;
