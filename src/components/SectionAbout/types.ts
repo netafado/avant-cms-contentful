3e221a17302068a0164aa1644b252c4aad787694
Isaias Santos <isaias.santos@boldcommerce.com>
2025-08-16 12:01:31 -0300

    feat: update web manifest with project name and remove unused SectionAbout component

--- src/components/SectionAbout/types.ts
+++
@@ -1,7 +0,0 @@
-import { SectionProps } from "../Section/types";
-
-export type SectionAboutProps = {
-  frontendTools: string[];
-  backendTools: string[];
-  integrationTools: string[];
-} & SectionProps;
